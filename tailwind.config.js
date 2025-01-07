cf938c802443a3766b6360d3c98da35885a9ee39
paduc <pad@dabla.com>
2025-01-07 18:39:21 +0100

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/client/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
