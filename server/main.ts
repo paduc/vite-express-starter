a261077f7f2b1b833e6be02d7d38ce9d860b79f4
Pierre-Antoine Duchateau <pad@dabla.com>
2025-01-18 05:56:34 +0100

    try other path

--- server/main.ts
+++ server/main.ts
@@ -4,15 +4,24 @@ const app = express();
 const port = Number(process.env.PORT) || 3000;
 const isProduction = process.env.NODE_ENV === 'production';
 
+app.get('/test', (req, res) => {
+  res.send('hello world')
+})
 
 if (isProduction) {
   console.log('isProduction');
 
   const assetsPath = '../app/dist'
-  // const files = fs.readdirSync(assetsPath)
-  // for(const file of files){
-  //   console.log(file)
-  // }
+  let files = fs.readdirSync(assetsPath)
+  for(const file of files){
+    console.log(assetsPath, file)
+  }
+
+  files = fs.readdirSync('../')
+  for(const file of files){
+    console.log('../' ,file)
+  }
+
   app.use(express.static(assetsPath));
 
   app.get('*', (req, res) => {
